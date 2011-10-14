f783a4e2ed4249d15d9210cedddcaa022441d800
Charley Robinson <charley@tokbox.com>
2011-10-13 18:13:46 -0700

    Added MIT license.

--- API_Config.php
+++ API_Config.php
@@ -1,26 +1,38 @@
 <?PHP
 
-/*!
+/**
 * OpenTok PHP Library
 * http://www.tokbox.com/
 *
-* Copyright 2010, TokBox, Inc.
+* Copyright (c) 2011, TokBox, Inc.
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the "Software"), 
+* to deal in the Software without restriction, including without limitation 
+* the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
 *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+* THE SOFTWARE.
 */
 
 class API_Config {
 
 	// Replace this value with your TokBox API Partner Key
-	const API_KEY = "";
+	const API_KEY = "5875231";
 
 	// Replace this value with your TokBox API Partner Secret
-	const API_SECRET = "";
+	const API_SECRET = "8f5cde4ade6b11ea22cfd73ea345c64b4e423d29";
 
-	const API_SERVER = "@staging.api.url@";
-	// Uncomment this line when you launch your app
-	// const API_SERVER = "@production.api.url@";
-
-
-	const SDK_VERSION = "tbphp-@sdk_version@.@opentok.sdk.php.mod_time@";
+	const API_SERVER = "http://staging.tokbox.com/hl";
+	//const API_SERVER = "https://api.opentok.com/hl";
 
 }
--- TestOpenTokSDK.php
+++ TestOpenTokSDK.php
@@ -1,26 +1,66 @@
 <?PHP
-/*!
-* OpenTok PHP Library v0.90.0
+/**
+* OpenTok PHP Library
 * http://www.tokbox.com/
 *
-* Copyright 2010, TokBox, Inc.
+* Copyright (c) 2011, TokBox, Inc.
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the "Software"), 
+* to deal in the Software without restriction, including without limitation 
+* the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
 *
-* Date: November 05 14:50:00 2010
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+* THE SOFTWARE.
 */
+
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 0);
+assert_options(ASSERT_QUIET_EVAL, 0);
+
+// Create a handler function
+function my_assert_handler($file, $line, $code)
+{
+    echo "<hr>Assertion Failed:
+        File '$file'<br />
+        Line '$line'<br />
+        Code '$code'<br /><hr />";
+}
+
+// Set up the callback
+assert_options(ASSERT_CALLBACK, 'my_assert_handler');
 
 require_once 'OpenTokSDK.php';
 $a = new OpenTokSDK(API_Config::API_KEY,API_Config::API_SECRET);
-print $a->generate_token();
-print "\n";
-print $a->generate_token('mysession');
-print "\n";
-print $a->generate_token('mysession', RoleConstants::MODERATOR);
-print "\n";
+$token = $a->generate_token();
+$token = $a->generate_token("mysession", RoleConstants::MODERATOR, 2);
+assert('$token');
+$a->validate_token($token);
+//phpinfo();
+//die();
+$token = $a->generate_token("mysession");
+$token = $a->generate_token("mysession", RoleConstants::SUBSCRIBER);
+$token = $a->generate_token("mysession", RoleConstants::PUBLISHER);
+$token = $a->generate_token("mysession", RoleConstants::MODERATOR);
+$token = $a->generate_token("mysession", "randomString");
+$token = $a->generate_token("mysession", RoleConstants::MODERATOR, gmmktime() - 100000);
+$token = $a->generate_token("mysession", RoleConstants::MODERATOR, gmmktime() + 100000);
+$token = $a->generate_token("mysession", RoleConstants::MODERATOR, gmmktime() + 1000000);
+
 try {
-	print $a->create_session('127.0.0.1')->getSessionId();
+	assert('$a->create_session("127.0.0.1")->getSessionId()');
 }catch(OpenTokException $e) {
 	print $e->getMessage();
- }
+	assert(false);
+}
 
-print "\n";
 
