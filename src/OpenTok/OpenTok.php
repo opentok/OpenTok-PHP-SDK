64ec13e5cbd889e5e97631e538adf69fa4c0c8dd
Ankur Oberoi <aoberoi@gmail.com>
2014-02-26 23:14:29 +1100

    refactors _do_request impl by using Guzzle\Http\Client

--- src/OpenTok/OpenTok.php
+++ src/OpenTok/OpenTok.php
@@ -29,6 +29,9 @@ namespace OpenTok;
 use OpenTok\Session;
 use OpenTok\Archive;
 
+use Guzzle\Http\Client;
+use Guzzle\Http\Exception\ClientErrorResponseException;
+
 define('OPENTOK_SDK_VERSION', '2.0.0-beta');
 define('OPENTOK_SDK_USER_AGENT', 'OpenTok-PHP-SDK/' . OPENTOK_SDK_VERSION);
 
@@ -50,11 +53,13 @@ class OpenTok {
     private $api_key;
     private $api_secret;
     private $server_url;
+    private $client;
 
     public function __construct($api_key, $api_secret) {
         $this->api_key = $api_key;
         $this->api_secret = $api_secret;
-        $this->server_url= 'https://api.opentok.com';
+        $this->client = new Client('https://api.opentok.com');
+        $this->client->setUserAgent(OPENTOK_SDK_USER_AGENT, true);
     }
 
     /** - Generate a token
@@ -253,74 +258,35 @@ class OpenTok {
 
     /** @internal */
     protected function _do_request($url, $data, $auth = array('type' => 'partner')) {
-        $url = $this->server_url . $url;
-
-        $dataString = "";
-        foreach($data as $key => $value){
-            $value = urlencode($value);
-            $dataString .= "$key=$value&";
-        }
-
-        $dataString = rtrim($dataString,"&");
-
         switch($auth['type']) {
             case 'token':
-                $authString = "X-TB-TOKEN-AUTH: ".$auth['token'];
+                $authHeaderName = 'X-TB-TOKEN-AUTH';
+                $authHeaderValue = $auth['token'];
                 break;
             case 'partner':
             default:
-                $authString = "X-TB-PARTNER-AUTH: $this->api_key:$this->api_secret";
+                $authHeaderName = 'X-TB-PARTNER-AUTH';
+                $authHeaderValue = $this->api_key . ':' . $this->api_secret;
                 break;
         }
 
-        //Use file_get_contents if curl is not available for PHP
-        if(function_exists("curl_init")) {
-            $ch = curl_init();
-
-            $api_key = $this->api_key;
-            $api_secret = $this->api_secret;
-
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_USERAGENT, OPENTOK_SDK_USER_AGENT);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, Array('Content-type: application/x-www-form-urlencoded'));
-            curl_setopt($ch, CURLOPT_HTTPHEADER, Array($authString));
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
-
-            $res = curl_exec($ch);
-            if (curl_errno($ch)) {
-                throw new RequestException('Request error: ' . curl_error($ch));
-            }
-
-            curl_close($ch);
-
-        } else {
-            if(function_exists("file_get_contents")) {
-                $context_source = array (
-                    'http' => array (
-                        'method' => 'POST',
-                        'header'=> Array(
-                            "Content-type: application/x-www-form-urlencoded",
-                            $authString,
-                            "Content-Length: " . strlen($dataString),
-                            'content' => $dataString
-                        ),
-                        'user_agent' => OPENTOK_SDK_USER_AGENT
-                    )
-                );
-                $context = stream_context_create($context_source);
-                $res = @file_get_contents( $url ,false, $context);
-            } else {
-                throw new RequestException("Your PHP installion neither supports the file_get_contents method nor cURL. Please enable one of these functions so that you can make API calls.");
-            }
+        $request = $this->client->post($url);
+        $request->addPostFields($data);
+        $request->addHeader($authHeaderName, $authHeaderValue);
+        try {
+            $response = $request->send();
+            return $response->getBody();
+        } catch (ClientErrorResponseException $e) {
+            echo 'Uh oh! ' . $e->getMessage();
+            echo 'HTTP request URL: ' . $e->getRequest()->getUrl() . "\n";
+            echo 'HTTP request: ' . $e->getRequest() . "\n";
+            echo 'HTTP response status: ' . $e->getResponse()->getStatusCode() . "\n";
+            echo 'HTTP response: ' . $e->getResponse() . "\n";
+            return;
         }
-        return $res;
     }
-    
-    
+
+
     /** - Old functions to be depreciated...
      */
     public function generate_token($session_id='', $role='', $expire_time=NULL, $connection_data='') {
