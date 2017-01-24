7b75553088453dda581acad47a05089947ba6dae
Taracque <taracque@taracque.hu>
2017-01-24 12:31:57 +0100

    Added forceDisconnect test cases

--- tests/OpenTok/OpenTokTest.php
+++ tests/OpenTok/OpenTokTest.php
@@ -833,6 +833,46 @@ class OpenTokTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("expired", $archive->status);
     }
 
+	public function testForceDisconnect()
+	{
+        // Arrange
+        $mock = new MockPlugin();
+        $response = MockPlugin::getMockFile(
+            self::$mockBasePath . 'v2/project/APIKEY/connection/CONNECTIONID/delete'
+        );
+        $mock->addResponse($response);
+        $this->client->addSubscriber($mock);
+
+        $connectionId = '063e72a4-64b4-43c8-9da5-eca071daab89';
+
+        // Act
+        $success = $this->opentok->forceDisconnect($connectionId);
+
+        // Assert
+        $requests = $mock->getReceivedRequests();
+        $this->assertCount(1, $requests);
+
+        $request = $requests[0];
+        $this->assertEquals('DELETE', strtoupper($request->getMethod()));
+        $this->assertEquals('/v2/project/'.$this->API_KEY.'/connection/'.$connectionId, $request->getPath());
+        $this->assertEquals('api.opentok.com', $request->getHost());
+        $this->assertEquals('https', $request->getScheme());
+
+        $contentType = $request->getHeader('Content-Type');
+        $this->assertNotEmpty($contentType);
+        $this->assertEquals('application/json', $contentType);
+
+        $authString = $request->getHeader('X-OPENTOK-AUTH');
+        $this->assertEquals(true, TestHelpers::validateOpenTokAuthHeader($this->API_KEY, $this->API_SECRET, $authString));
+
+        // TODO: test the dynamically built User Agent string
+        $userAgent = $request->getHeader('User-Agent');
+        $this->assertNotEmpty($userAgent);
+        $this->assertStringStartsWith('OpenTok-PHP-SDK/2.4.1-alpha.1', $userAgent->__toString());
+
+        $this->assertTrue($success);
+    }
+
     public function testStartsBroadcast()
     {
         // Arrange
