8696ac92d33cdc1f3dc32e24ba24f5c1e5da03f2
magroski <magroski@gmail.com>
2018-07-12 09:14:35 -0300

    adds sessionId to listArchives api call

--- src/OpenTok/Util/Client.php
+++ src/OpenTok/Util/Client.php
@@ -247,7 +247,7 @@ class Client
         return true;
     }
 
-    public function listArchives($offset, $count)
+    public function listArchives($offset, $count, $sessionId)
     {
         $request = new Request('GET', '/v2/project/'.$this->apiKey.'/archive');
         $queryParams = [];
@@ -256,6 +256,9 @@ class Client
         }
         if (!empty($count)) {
             $queryParams['count'] = $count;
+        }
+        if (!empty($sessionId)) {
+            $queryParams['sessionId'] = $sessionId;
         }
         try {
             $response = $this->client->send($request, [
