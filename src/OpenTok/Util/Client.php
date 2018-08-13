b82aa40526b270b663b698a61fc65b045a8f6a20
Manik <msach22@gmail.com>
2018-08-13 10:37:43 -0700

    Update to version 4.2.0

--- src/OpenTok/Util/Client.php
+++ src/OpenTok/Util/Client.php
@@ -38,7 +38,7 @@ use OpenTok\MediaMode;
 
 // TODO: build this dynamically
 /** @internal */
-define('OPENTOK_SDK_VERSION', '4.1.2-alpha.1');
+define('OPENTOK_SDK_VERSION', '4.2.0');
 /** @internal */
 define('OPENTOK_SDK_USER_AGENT', 'OpenTok-PHP-SDK/' . OPENTOK_SDK_VERSION);
 
