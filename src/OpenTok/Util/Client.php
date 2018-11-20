4690857aec6b4f45c970a89e32d5240fd035eaa5
Matthieu <matthieu@learnybox.com>
2018-11-20 15:09:18 +0100

    send options like maxDuration and resolution to startBroadcast

--- src/OpenTok/Util/Client.php
+++ src/OpenTok/Util/Client.php
@@ -280,13 +280,17 @@ class Client
             '/v2/project/'.$this->apiKey.'/broadcast'
         );
 
+        $optionsJson = [
+            'sessionId' => $sessionId,
+            'layout' => $options['layout']->jsonSerialize()
+        ];
+        unset($options['layout']);
+        $optionsJson = array_merge($optionsJson, $options);
+
         try {
             $response = $this->client->send($request, [
                 'debug' => $this->isDebug(),
-                'json' => [
-                    'sessionId' => $sessionId,
-                    'layout' => $options['layout']->jsonSerialize()
-                ]
+                'json' => $optionsJson
             ]);
             $broadcastJson = json_decode($response->getBody(), true);
         } catch (\Exception $e) {
