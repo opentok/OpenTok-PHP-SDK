18c51a560ae59ed4cb5f70ba1f49628d6eb326bd
Jeff Swartz <jeff.swartz@vonage.com>
2021-10-15 16:23:33 -0700

    Docs corrections

--- src/OpenTok/Role.php
+++ src/OpenTok/Role.php
@@ -20,9 +20,10 @@ abstract class Role extends BasicEnum
     */
     const PUBLISHER = 'publisher';
     /**
-    * In addition to the privileges granted to a publisher, in clients using the OpenTok.js
-    * library, a moderator can call the forceUnpublish() and forceDisconnect() methods of
-    * the Session object.
+    * In addition to the privileges granted to a publisher, a moderator can perform
+    * moderation functions, such as forcing clients to disconnect, to stop publishing streams,
+    * or to mute audio in published streams. See the
+    * <a href="https://tokbox.com/developer/guides/moderation/">Moderation developer guide</a>.
     */
     const MODERATOR = 'moderator';
 }
