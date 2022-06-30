c3630fccc553d2a9fc421bc058881b4b671f7b94
Remi Gaillard <>
2022-06-30 09:02:08 +0200

    observeForceMute attribute is not returned by the /call endpoint

--- src/OpenTok/SipCall.php
+++ src/OpenTok/SipCall.php
@@ -32,7 +32,6 @@ class SipCall
         $this->data['id'] = $sipCallData['id'];
         $this->data['connectionId'] = $sipCallData['connectionId'];
         $this->data['streamId'] = $sipCallData['streamId'];
-        $this->data['observeForceMute'] = $sipCallData['observeForceMute'];
     }
 
     /**
@@ -45,7 +44,6 @@ class SipCall
             case 'id':
             case 'connectionId':
             case 'streamId':
-            case 'observeForceMute':
                 return $this->data[$name];
             default:
                 return null;
